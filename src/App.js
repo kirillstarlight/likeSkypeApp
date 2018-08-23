8774c429bbf0d7199222e1bcb48d2c446c53fc64
kirillstarlight <kirillstarlight@gmail.com>
2018-08-23 16:24:54 +0300

    add init styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { React } from 'react';
 
-class App extends Component {
-  render() {
+import './App.css';
+import Sidebar from './components/main';
+import Main from './components/main';
+
+const  App = () = {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Sidebar/>
+        <Main/>
       </div>
     );
-  }
 }
 
 export default App;
